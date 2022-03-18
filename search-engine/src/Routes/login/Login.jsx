43691aaef807e9cd160044d3549c2ba12e42e01e
NONO0N0N <72507375+NONO0N0N@users.noreply.github.com>
2022-03-18 17:24:02 +0000

    fix redirect bug in login and signup page

--- search-engine/src/Routes/login/Login.jsx
+++ search-engine/src/Routes/login/Login.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import loginAPI from "../../api/login";
 import UserDetailAPI from "../../api/userDetail";
-import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { log_in, mod_token, open_alert, set_detail } from "../../redux/action";
 import { BiLockOpenAlt } from "react-icons/bi";
 import { BiUser } from "react-icons/bi";
@@ -17,7 +18,18 @@ function Login() {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [remember, setRemember] = useState(false);
+
+  const isLogin = useSelector(state => state.LoginReducer);
   const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  // if the user is login they should not be able to redirect to this page
+  useEffect(() => {
+    if (isLogin) {
+      navigate(`/`);
+    }
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -30,17 +42,19 @@ function Login() {
   function migrateValue(token) {
     dispatch(mod_token(token));
     UserDetailAPI.getCurrentUserDetail(token)
-      .then((response) =>
+      .then((response) => {
         dispatch(
           set_detail(response.username, response.email, response.displayname, response.public_id)
-        )
+        );
+        dispatch(log_in());
+      }
       )
       .catch((error) => console.log("error", error));
-    dispatch(log_in());
-    dispatch(open_alert(true,
-      "Logged in",
-      "User has been sucessfully logged in to the system",
-      "close"));
+
+    // dispatch(open_alert(true,
+    //   "Logged in",
+    //   "User has been sucessfully logged in to the system",
+    //   "close"));
   }
 
   function modifyValue(name, value) {
@@ -52,49 +66,53 @@ function Login() {
   }
 
   return (
-    <div>
-      <Alert />
-
-      <Head isSearch={true} />
-
-      <form id="login" onSubmit={handleSubmit}>
-        <h1>Login</h1>
-        <LoginParam
-          name="Username"
-          IconContext={<BiUser />}
-          type="text"
-          value={userName}
-          placeholder="Type your username"
-          modifyValue={modifyValue}
-        />
-        <LoginParam
-          name="Password"
-          IconContext={<BiLockOpenAlt />}
-          type="password"
-          value={password}
-          placeholder="Type your password"
-          modifyValue={modifyValue}
-        />
-
-        <div className="reset-password ">forgot password</div>
-
-        <div className="remember-me">
-          <input
-            type="checkbox"
-            id="remember-me"
-            name="remember-me"
-            value={remember}
-            onClick={() => {
-              setRemember(!remember);
-            }}
+    (
+      <div>
+        <Alert />
+  
+        <Head isSearch={true} />
+  
+        <form id="login" onSubmit={handleSubmit}>
+          <h1>Login</h1>
+          <LoginParam
+            name="Username"
+            IconContext={<BiUser />}
+            type="text"
+            value={userName}
+            placeholder="Type your username"
+            modifyValue={modifyValue}
           />
-          <p>remember me</p>
-        </div>
-
-        <Button name="login" />
-      </form>
-    </div>
+          <LoginParam
+            name="Password"
+            IconContext={<BiLockOpenAlt />}
+            type="password"
+            value={password}
+            placeholder="Type your password"
+            modifyValue={modifyValue}
+          />
+  
+          <div className="reset-password ">forgot password</div>
+  
+          <div className="remember-me">
+            <input
+              type="checkbox"
+              id="remember-me"
+              name="remember-me"
+              value={remember}
+              onClick={() => {
+                setRemember(!remember);
+              }}
+            />
+            <p>remember me</p>
+          </div>
+  
+          <Button name="login" />
+        </form>
+      </div>
+    )
   );
+
+  
 }
 
 export default Login;
