69942ca6464d1a146609591611d3bc91e63c416f
NONO0N0N <72507375+NONO0N0N@users.noreply.github.com>
2022-03-20 07:20:43 +0000

    refactor: remove unimportant data to localstorage

--- search-engine/src/Routes/test/Test.jsx
+++ search-engine/src/Routes/test/Test.jsx
@@ -1,11 +1,21 @@
 import Head from "../../Components/head/Head";
 import Alert from "../../Components/alert/alert";
 
+import { useCookies } from 'react-cookie';
+
+
 function Test() {
+    const [cookies, setCookie] = useCookies(['name']);
+  
+    setCookie("abc", 6555);
+    setCookie("def", 6555);
+  
     return (
         <div>
             <Head isSearch={true} />
             <Alert isOpen={true} header='error' content='wrong password' buttonContent='close'/>
+            <p>{cookies.abc}</p>
+            <p>{cookies.def}</p>
         </div>
     )
 }
