4a68a59dc8dab09a684d8d6d89450494053d30c4
NONO0N0N <72507375+NONO0N0N@users.noreply.github.com>
2022-03-30 15:24:25 +0100

    tag

--- search-engine/src/Components/ResultList.js
+++ search-engine/src/Components/ResultList.js
@@ -1,44 +1,53 @@
 import SearchResult from './searchResult/SearchResult';
 
 const ResultList = (props) => {
+        // By Tim Widmayer
 
-    
-    // props.searchID
-    // console.log(props.results);
+        // props.searchID
+        // console.log(props.results);
 
-    // console.log("OBJ IS:", Object);
-    // console.log(Object.entries(props.results));
+        // console.log("OBJ IS:", Object);
+        // console.log(Object.entries(props.results));
 
-    if(Object.entries(props.results).length === 0) {
-        return (<p> No results found. Seems like we have to work on our search engine.</p>)
-    } else {
-        return (
-            <div className="row">
-            {/* 
-                Display the first 20 results if props.results is not None.
-                Currently the "results" object has the format key: value, where
-                    - key is the result index (starting at 0)
-                    - value is an object representing the material with properties
-                        - id: int
-                        - title: str
-                        - type: str
-                        - language: str
-                        - keywords: [str]
-                        - concepts: [str]
-                        - description: str
-                        - url: str
-            */}
-            {props.results && Object.entries(props.results).slice(0,20).map(([key, material], index) => {
-                return (
-                    <div key= {key} className="col-md-6 ">
-                        <SearchResult material={material} index={(index+1)} searchID={props.searchID}/>
-                    </div>
+        if (Object.entries(props.results).length === 0) {
+            return ( < p > No results found.Seems like we have to work on our search engine. < /p>)
+            }
+            else {
+                return ( <
+                    div className = "row" > {
+                        /* 
+                                        Display the first 20 results if props.results is not None.
+                                        Currently the "results" object has the format key: value, where
+                                            - key is the result index (starting at 0)
+                                            - value is an object representing the material with properties
+                                                - id: int
+                                                - title: str
+                                                - type: str
+                                                - language: str
+                                                - keywords: [str]
+                                                - concepts: [str]
+                                                - description: str
+                                                - url: str
+                                    */
+                    } {
+                        props.results && Object.entries(props.results).slice(0, 20).map(([key, material], index) => {
+                            return ( <
+                                div key = { key }
+                                className = "col-md-6 " >
+                                <
+                                SearchResult material = { material }
+                                index = {
+                                    (index + 1) }
+                                searchID = { props.searchID }
+                                /> <
+                                /div>
+                            )
+
+                        })
+                    } <
+                    /div>
                 )
-                
-                })}
-            </div>
-            )
-    }
-}
+            }
+        }
 
-export default ResultList;
+        export default ResultList;
